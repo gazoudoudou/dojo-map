b9e318504ae1377291ceb1a6138be3461cabf8db
gazoudoudou <gaspardd@MBP-de-Gaspard.bam.lan>
2019-02-08 18:39:38 +0100

    Display map on Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,24 +1,20 @@
 // @flow
 
 import React, { PureComponent } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { withNavigationFocus } from 'react-navigation';
+import RNMapView from 'react-native-maps';
 
 class Home extends PureComponent {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>{'coucou'}</Text>
-      </View>
-    );
+    return <RNMapView style={styles.container} showsUserLocation={this.props.isFocused} />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
-export default Home;
+export default withNavigationFocus(Home);
